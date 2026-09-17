c51b9ea9f04a18211719e54131a6d52fd9028005
jrodriguez29-JLR <jrodriguez29@setoncatholic.org>
2026-09-17 21:41:59 +0000

    change 3

--- Main.py
+++ Main.py
@@ -1,2 +1,12 @@
-Name = input("What is your name?:")
-print("hello", Name)
+Name = input("What is your name?: ")
+Age = int(input("How old are you?: "))
+color = input("What is your favorite color?: ")
+
+print("Hello", Name, "you are", Age, "and your favorite color is", color)
+
+if Age >= 18:
+    print("You are an adult!")
+elif Age >= 13:
+    print("You are a teenager!")
+else:
+    print("You are a child!")
